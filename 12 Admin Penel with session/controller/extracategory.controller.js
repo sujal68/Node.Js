b5d9b6a2c34aabce5c1b7fdb1a6fa8e669d11409
sujal68 <sujalkidecha68@gmail.com>
2026-02-15 14:13:07 +0530

    extra category

--- 12 Admin Penel with session/controller/extracategory.controller.js
+++ 12 Admin Penel with session/controller/extracategory.controller.js
@@ -1,5 +1,6 @@
 const Category = require('../model/categories.model');
 const SubCategory = require('../model/subCategories.model');
+const ExtraCategory = require('../model/extraCategories.model');
 
 module.exports.addExtraCategoryPage = async (req, res) => {
     try {
@@ -14,3 +15,77 @@ module.exports.addExtraCategoryPage = async (req, res) => {
     }
 }
 
+module.exports.viewExtraCategoryPage = async (req, res) => {
+    try {
+        const admin = res.locals.admin;
+        const allExtraCategory = await ExtraCategory.find().populate('category_id').populate('subcategory_id');
+        return res.render('extracategory/viewExtracategoryPage', { admin, currentPath: req.originalUrl.split('?')[0], allExtraCategory });
+    } catch (error) {
+        console.log('Error : ', error);
+        req.flash('error', 'Something went wrong !!');
+        return res.redirect('/extraCategory/viewExtraCategoryPage');
+    }
+}
+
+module.exports.addExtraCategory = async (req, res) => {
+    try {
+        const newExtraCategory = await ExtraCategory.create(req.body);
+
+        if (newExtraCategory) {
+            req.flash('success', 'Extra Category added successfully !!');
+        } else {
+            req.flash('error', 'Failed to add Extra Category !!');
+        }
+
+        return res.redirect('/extraCategory/addExtraCategoryPage');
+    } catch (error) {
+        console.log('Error : ', error);
+        req.flash('error', 'Something went wrong !!');
+        return res.redirect('/extraCategory/addExtraCategoryPage');
+    }
+}
+
+module.exports.deleteExtraCategory = async (req, res) => {
+    try {
+        const deleted = await ExtraCategory.findByIdAndDelete(req.params.id);
+        if (deleted) {
+            req.flash('success', `${deleted.extraCategory_name} Extra Category Deleted Successfully..`);
+        } else {
+            req.flash('error', 'Extra Category Deletion Failed..');
+        }
+        return res.redirect('/extraCategory/viewExtraCategoryPage');
+    } catch (error) {
+        console.log('Error : ', error);
+        req.flash('error', 'Something went wrong !!');
+        return res.redirect('/extraCategory/viewExtraCategoryPage');
+    }
+}
+
+module.exports.editExtraCategoryPage = async (req, res) => {
+    try {
+        const extraCategoryData = await ExtraCategory.findById(req.params.id).populate('category_id').populate('subcategory_id');
+        const allCategory = await Category.find();
+        const allSubCategory = await SubCategory.find();
+        return res.render('extracategory/editExtracategoryPage', { extraCategoryData, allCategory, allSubCategory, currentPath: req.originalUrl.split('?')[0] });
+    } catch (error) {
+        console.log('Error : ', error);
+        req.flash('error', 'Something went wrong !!');
+        return res.redirect('/extraCategory/viewExtraCategoryPage');
+    }
+}
+
+module.exports.editExtraCategory = async (req, res) => {
+    try {
+        const updated = await ExtraCategory.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (updated) {
+            req.flash('success', `${updated.extraCategory_name} Extra Category Updated Successfully..`);
+        } else {
+            req.flash('error', 'Extra Category Updation Failed..');
+        }
+        return res.redirect('/extraCategory/viewExtraCategoryPage');
+    } catch (error) {
+        console.log('Error : ', error);
+        req.flash('error', 'Something went wrong !!');
+        return res.redirect('/extraCategory/viewExtraCategoryPage');
+    }
+}
--- 12 Admin Penel with session/routes/extraCategory.route.js
+++ 12 Admin Penel with session/routes/extraCategory.route.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { addExtraCategoryPage } = require('../controller/extracategory.controller');
+const { addExtraCategoryPage, addExtraCategory, viewExtraCategoryPage, deleteExtraCategory, editExtraCategoryPage, editExtraCategory } = require('../controller/extracategory.controller');
 
 const extraCategoryRoutes = express.Router();
 
-extraCategoryRoutes.get('/addExtraCategoryPage', addExtraCategoryPage)
+extraCategoryRoutes.get('/addExtraCategoryPage', addExtraCategoryPage);
+extraCategoryRoutes.post('/addExtraCategory', addExtraCategory);
+extraCategoryRoutes.get('/viewExtraCategoryPage', viewExtraCategoryPage);
+extraCategoryRoutes.get('/deleteExtraCategory/:id', deleteExtraCategory);
+extraCategoryRoutes.get('/editExtraCategory/:id', editExtraCategoryPage);
+extraCategoryRoutes.post('/editExtraCategory/:id', editExtraCategory);
 
 module.exports = extraCategoryRoutes;
