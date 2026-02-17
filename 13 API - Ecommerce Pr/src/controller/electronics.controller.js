849d3816e86fdc33cd6773a2c398e623588edc7d
sujal68 <sujalkidecha68@gmail.com>
2026-02-17 15:10:57 +0530

    API

--- 13 API - Ecommerce Pr/src/controller/electronics.controller.js
+++ 13 API - Ecommerce Pr/src/controller/electronics.controller.js
@@ -9,6 +9,7 @@ module.exports.AddProducts = async (req, res) => {
         });
     } catch (err) {
         return res.status(500).json({
+            status: 500,
             error: true,
             message: "Something Went Wrong",
             error: err,
@@ -26,9 +27,68 @@ module.exports.GetProducts = async (req, res) => {
         });
     } catch (err) {
         return res.status(500).json({
+            status: 500,
             error: true,
             message: "Something Went Wrong",
             error: err,
         });
     }
 };
+
+module.exports.DeleteProducts = async (req, res) => {
+    try {
+        console.log('Delete request id:', req.query.id);
+
+        const deletedProduct = await electronics.findByIdAndDelete(req.query.id, { new: true });
+
+        if (!deletedProduct) {
+            return res.status(404).json({
+                status: 404,
+                error: true,
+                message: "Products Deletetion Failed!!",
+            });
+        }
+        return res.status(201).json({
+            status: 201,
+            error: false,
+            message: "Products Deletetion Successfully",
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            status: 500,
+            error: true,
+            message: "Something Went Wrong",
+            error: err,
+        });
+    }
+}
+
+module.exports.UpdateProducts = async (req, res) => {
+    try {
+
+        const updatedProducts = await electronics.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (!updatedProducts) {
+            return res.status(404).json({
+                status: 404,
+                error: true,
+                message: "Products Updation Failed!!",
+            });
+        };
+
+        return res.status(201).json({
+            status: 201,
+            error: false,
+            message: "Product Updated Yeeee!!"
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            status: 500,
+            error: true,
+            message: "Something Went Wrong",
+            error: err,
+        });
+    }
+}
--- 13 API - Ecommerce Pr/src/routes/electronics.routes.js
+++ 13 API - Ecommerce Pr/src/routes/electronics.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { AddProducts, GetProducts } = require('../controller/electronics.controller');
+const { AddProducts, GetProducts, DeleteProducts, UpdateProducts } = require('../controller/electronics.controller');
 
 const electroRoutes = express.Router();
 
 electroRoutes.post('/', AddProducts);
 electroRoutes.get('/', GetProducts);
-
+electroRoutes.delete('/', DeleteProducts);
+electroRoutes.patch('/:id', UpdateProducts)
 module.exports = electroRoutes;
