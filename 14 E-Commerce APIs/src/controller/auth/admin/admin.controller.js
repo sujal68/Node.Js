075e8d32c3b304f49d1187a1152bdd1798d2142e
sujal68 <sujalkidecha68@gmail.com>
2026-02-20 15:42:06 +0530

    password encrypt

--- 14 E-Commerce APIs/src/controller/auth/admin/admin.controller.js
+++ 14 E-Commerce APIs/src/controller/auth/admin/admin.controller.js
@@ -1,20 +1,23 @@
-
 const AdminAuthService = require('../../../services/auth/admin/admin.service');
 const { successResponse, errorResponse } = require('../../../utils/response');
 const { MSG } = require('../../../utils/msg');
 const moment = require('moment');
+const statusCode = require('http-status-codes');
+const bcrypt = require('bcrypt');
 
 const adminAuthService = new AdminAuthService();
 
 module.exports.registerAdmins = async (req, res) => {
     try {
+        req.body.password = await bcrypt.hash(req.body.password, 11);
+
         req.body.createAt = moment().format('YYYY-MM-DD HH:mm:ss');
         req.body.updateAt = moment().format('YYYY-MM-DD HH:mm:ss');
         const newAdmin = await adminAuthService.registerAdmin(req.body);
         if (!newAdmin) {
-            return res.status(400).json(successResponse(400, true, MSG.Admin_Registration_Failed, newAdmin));
+            return res.status(statusCode.BAD_REQUEST).json(successResponse(statusCode.BAD_REQUEST, true, MSG.Admin_Registration_Failed, newAdmin));
         }
-        return res.status(201).json(errorResponse(201, false, MSG.Admin_Registration_Success));
+        return res.status(statusCode.CREATED).json(errorResponse(statusCode.CREATED, false, MSG.Admin_Registration_Success));
     } catch (error) {
         console.log("Something Went Wrong!!", error);
     }
